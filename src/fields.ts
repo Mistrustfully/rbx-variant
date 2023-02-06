98f663034abbab4b028e6c15bdf52374558d6e24
Mistrustfully <mistrustfully@gmail.com>
2023-02-05 16:25:19 -1100

    Add fields overload

--- src/fields.ts
+++ src/fields.ts
@@ -1,7 +1,10 @@
 import { Identity } from "./types";
 
-export function fields<T>(defaults: Partial<T> = {}): (input: T) => Identity<T> {
-	return (input: T): Identity<T> => {
+export function fields<T>(): (input: T) => Identity<T>;
+export function fields<T>(defaults: T): (input: Partial<T>) => Identity<T>;
+
+export function fields<T>(defaults?: T): (input: Partial<T> | T) => Identity<T> {
+	return (input): Identity<T> => {
 		return { ...defaults, ...input } as Identity<T>;
 	};
 }
